539be2d977f235b84c92d3dbcf943d9368938c40
liliul <you@example.com>
2019-01-28 23:00:00 -0200

    vue

--- src/components/Conversor.vue
+++ src/components/Conversor.vue
@@ -31,7 +31,9 @@ export default {
                   return res.json();
               })
               .then(json => {
+                  console.log(json);
                   const cotacao = json[de_para].val;
+                  console.log("asa");
                   this.moedaB_value = (cotacao * parseFloat(this.moedaA_value)).toFixed(2);
               });
         }
